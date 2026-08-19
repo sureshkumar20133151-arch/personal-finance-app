5835169e0b821e6e8f7de3488754ffbb3ea459f4
Suresh Kumar <sureshkumar20133151@gmail.com>
2026-08-18 21:57:36 -0700

    build: compile native Android app with JDK 21, remove SMS permissions, disable notification listener service, and fix TLS handshake

--- android/app/src/main/java/com/budgettracker/app/MainActivity.java
+++ android/app/src/main/java/com/budgettracker/app/MainActivity.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.ionicframework.capacitor.Checkout;
-import ai.soliman.plugins.messagereader.MessageReaderPlugin;
 import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // Register custom Capacitor plugins
-        registerPlugin(NotificationListenerPlugin.class);
         registerPlugin(GoogleAuth.class);
         registerPlugin(Checkout.class);
-        registerPlugin(MessageReaderPlugin.class);
         registerPlugin(LocalNotificationsPlugin.class);
         super.onCreate(savedInstanceState);
     }
